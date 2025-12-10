8386e45092c7ebedc25d69f4d6e879c5ab1d6d64
kholisiswa <kholis.is26@gmail.com>
2025-12-10 09:15:33 +0700

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -135,7 +135,7 @@ export default function Register() {
               <img
                 src="favicon.png"
                 alt={APP_TITLE}
-                className="w-8 h-8 rounded-full shadow-lg object-cover"
+                className="w-16 h-16 rounded-full shadow-lg object-cover"
               />
             </div>
             <h1 className="text-3xl font-bold text-foreground mb-2">
